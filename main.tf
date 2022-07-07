966e4abeee6b1477c962f9249741e51f3d241652
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-07 13:19:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ data "azurerm_resource_group" "RG" {
   name = var.resource_group_name
 }
 
+data "azurerm_key_vault" "KeyVault" {
+  name = var.name_keyvault
+}
+
 # Generate Ramdon String
 resource "random_string" "random_user" {
   length                       = "14"
@@ -11,5 +15,5 @@ resource "random_string" "random_user" {
 resource "azurerm_key_vault_secret" "user_secret" {
   name                         = var.usersecret_name
   value                        = random_string.random_user.result
-  key_vault_id                 = data.azurerm_key_vault.RG.id
+  key_vault_id                 = data.azurerm_key_vault.KeyVault.id
 }
